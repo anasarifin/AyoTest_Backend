721a81d50cd198e3192ff359240bd99dffb07312
verrygustiandrea <verrygustiandrea@gmail.com>
2020-02-29 15:57:57 +0700

    CRUD assessment

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ const Route = express.Router();
 // import all routes
 const admin = require("./routes/admin");
 const question = require("./routes/question");
+const assessment_name = require("./routes/assessment_name");
 // Routes
 Route.use("/admin", admin);
 Route.use("/question", question);
+Route.use("/assessment", assessment_name);
 
 module.exports = Route;
