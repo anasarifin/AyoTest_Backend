cf1049916e1c124ad01526359c43fb7d63b44ad1
rahimatunnimah <rahimatunnimah@gmail.com>
2020-03-01 21:17:58 +0700

    hapus file

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ const Route = express.Router()
 
 // import all routes
 const users = require('./routes/users')
-// const score_user = require('./routes/score_user')
 const assessment_name = require("./routes/assessment_name");
+
 // Routes
 Route.use('/users', users)
-// Route.use('/score', score_user)
 Route.use("/assessment", assessment_name);
 module.exports = Route
