ce1372c94126df2ffc4a5f38dfef3cf1e52dd46d
verrygustiandrea <verrygustiandrea@gmail.com>
2020-03-02 16:44:27 +0700

    get question json fix

--- src/models/question.js
+++ src/models/question.js
@@ -3,12 +3,39 @@ const fs = require("fs");
 
 module.exports = {
   getAllQuestion: () => {
+    let data = [];
+    let no = 0;
     return new Promise((resolve, reject) => {
       conn.query(
-        "SELECT  assessment_name.name, assessment_name.code ,bank_question_master.question, bank_question_master.choice_1, bank_question_master.choice_2, bank_question_master.choice_3, bank_question_master.choice_4, bank_question_master.choice_5, bank_question_master.answer_correct FROM `bank_question_master` INNER JOIN assessment_name ON bank_question_master.id_assessment_name=assessment_name.id_assessment WHERE bank_question_master.deleted = 0",
+        "SELECT  assessment_name.name, assessment_name.code ,bank_question_master.question, bank_question_master.choice_1, bank_question_master.choice_2, bank_question_master.choice_3, bank_question_master.choice_4, bank_question_master.choice_5 FROM `bank_question_master` INNER JOIN assessment_name ON bank_question_master.id_assessment_name=assessment_name.id_assessment WHERE bank_question_master.deleted = 0",
         (err, result) => {
+          console.log(result);
+          const data = result.map((x, index) => {
+            return {
+              no: index + 1,
+              question: x.question,
+              answer: [
+                { label: x.choice_1, value: 1 },
+                { label: x.choice_2, value: 2 },
+                { label: x.choice_3, value: 3 },
+                { label: x.choice_4, value: 4 },
+                { label: x.choice_5, value: 5 }
+              ]
+            };
+          });
+          // result.map(e => {
+          //   data.no_soal = no + 1;
+          //   data.question = e.question;
+          //   data.answer = [
+          //     { label_1: e.choice_1, value: 1 },
+          //     { label_2: e.choice_2, value: 2 },
+          //     { label_3: e.choice_3, value: 3 },
+          //     { label_4: e.choice_4, value: 4 },
+          //     { label_5: e.choice_5, value: 5 }
+          //   ];
+          // });
           if (!err) {
-            resolve(result);
+            resolve(data);
           } else {
             reject(err);
           }
@@ -18,13 +45,27 @@ module.exports = {
   },
 
   getQuestionById: id_question => {
+    let data = {};
     return new Promise((resolve, reject) => {
       conn.query(
-        "SELECT  assessment_name.name, assessment_name.code ,bank_question_master.question, bank_question_master.choice_1, bank_question_master.choice_2, bank_question_master.choice_3, bank_question_master.choice_4, bank_question_master.choice_5, bank_question_master.answer_correct FROM `bank_question_master` INNER JOIN assessment_name ON bank_question_master.id_assessment_name=assessment_name.id_assessment WHERE bank_question_master.deleted = 0 AND bank_question_master.id_bank_question_master=?",
+        "SELECT  assessment_name.name, assessment_name.code ,bank_question_master.question, bank_question_master.choice_1, bank_question_master.choice_2, bank_question_master.choice_3, bank_question_master.choice_4, bank_question_master.choice_5 FROM `bank_question_master` INNER JOIN assessment_name ON bank_question_master.id_assessment_name=assessment_name.id_assessment WHERE bank_question_master.deleted = 0 AND bank_question_master.id_bank_question_master=?",
         id_question,
         (err, result) => {
+          const data = result.map((x, index) => {
+            return {
+              no: index + 1,
+              question: x.question,
+              answer: [
+                { label: x.choice_1, value: 1 },
+                { label: x.choice_2, value: 2 },
+                { label: x.choice_3, value: 3 },
+                { label: x.choice_4, value: 4 },
+                { label: x.choice_5, value: 5 }
+              ]
+            };
+          });
           if (!err) {
-            resolve(result);
+            resolve(data);
           } else {
             reject(err);
           }
