2d3c32ff35299b3166712fa9dfea99771c73bea2
dhifaagh <mac@mac.local>
2020-03-04 01:44:10 +0700

    conflict verry3

--- src/models/assessment_name.js
+++ src/models/assessment_name.js
@@ -2,6 +2,18 @@ const conn = require("../config/db");
 const fs = require("fs");
 
 module.exports = {
+    getAllAssessmentName: () => {
+        return new Promise((resolve, reject) => {
+            conn.query("SELECT  * FROM assessment_name", (err, result) => {
+                if (!err) {
+                    resolve(result);
+                } else {
+                    reject(err);
+                }
+            });
+        });
+    },
+
     getAllAssessmentNameById: id_assessment => {
         return new Promise((resolve, reject) => {
             conn.query(
@@ -86,6 +98,21 @@ module.exports = {
     });
   },
 
+    getAllAssessmentNameByIdAdmin: id_admin => {
+        return new Promise((resolve, reject) => {
+            conn.query(
+                "SELECT  * FROM assessment_name WHERE id_admin=?",
+                id_admin,
+                (err, result) => {
+                    if (!err) {
+                        resolve(result);
+                    } else {
+                        reject(err);
+                    }
+                }
+            );
+        });
+    },
 
     insertAssessmentName: data => {
         return new Promise((resolve, reject) => {
@@ -169,7 +196,40 @@ module.exports = {
             );
         });
     },
- // merge from hima (search assessment)
+    // merge from hima (search assessment)
+    searchAssessment: name => {
+        return new Promise((resolve, reject) => {
+            console.log("name", name);
+            conn.query(
+                `SELECT an.id_assessment, an.name, a.id_admin, a.name as admin_name FROM assessment_name as an LEFT JOIN admin as a ON an.id_admin=a.id_admin WHERE an.name LIKE '%${name}%'`,
+                (err, result) => {
+                    console.log(result);
+                    if (!err) {
+                        resolve(result);
+                    } else {
+                        reject(new Error(err));
+                    }
+                }
+            );
+        });
+    },
+  hideAssessmentName: data => {
+    // console.log(id_assessment);
+    return new Promise((resolve, reject) => {
+      conn.query(
+        "UPDATE assessment_name SET hide=? WHERE id_assessment = ?",
+        [data.hide, data.id_assessment],
+        (err, result) => {
+          if (!err) {
+            resolve(result);
+          } else {
+            reject(err);
+          }
+        }
+      );
+    });
+  },
+  // merge from hima (search assessment)
   searchAssessment: name => {
     return new Promise((resolve, reject) => {
       console.log("name", name);
@@ -185,5 +245,7 @@ module.exports = {
         }
       );
     });
-  }
+      
+      }
 };
+
